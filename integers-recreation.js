2cbdfc3786a1ebc80603bd1369d0f997b2ce5547
Myles Dow <spence.dow@hotmail.com>
2023-02-07 20:40:06 -0500

    integers-recreation

---
+++ integers-recreation.js
@@ -0,0 +1,29 @@
+/*
+1, 246, 2, 123, 3, 82, 6, 41 are the divisors of number 246. Squaring these divisors we get: 1, 60516, 4, 15129, 9, 6724, 36, 1681. The sum of these squares is 84100 which is 290 * 290.
+Task
+
+Find all integers between m and n (m and n integers with 1 <= m <= n) such that the sum of their squared divisors is itself a square.
+
+We will return an array of subarrays or of tuples (in C an array of Pair) or a string. The subarrays (or tuples or Pairs) will have two elements: first the number the squared divisors of which is a square and then the sum of the squared divisors.
+Example:
+
+list_squared(1, 250) --> [[1, 1], [42, 2500], [246, 84100]]
+list_squared(42, 250) --> [[42, 2500], [246, 84100]]
+*/
+function listSquared(m, n) {
+    let results = []
+    for (let i = m; i <= n; i++) {
+      let divisors
+      divisors = i === 1 ?  [1] : [1, i] 
+      for (let j = 2; j <= i/2; j++) {
+        if (i % j === 0) {
+          divisors.push(j)
+        }
+      }
+      let sum = divisors.reduce((acc, ele) => acc + ele ** 2 ,0)
+      if (Number.isInteger(Math.sqrt(sum))) {
+        results.push([i, sum])
+      }
+    }
+    return results
+}
